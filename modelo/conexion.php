38706ac637cbcd900a8aa6f7ee50932a9e4ba822
WAR029 <wilmer.andres.romero1991@gmail.com>
2025-01-02 15:25:29 -0500

    se crea el proyecto login_avanzado con una estructura mvc utilizando php puro creando las estructuras basica de un login y una pagina de inicio de session

---
+++ modelo/conexion.php
@@ -0,0 +1,10 @@
+<?php
+
+
+$conexion = new mysqli("localhost", "warg", "123456", "login"); //si mi base de datos o mi servidor web esta en la nube colocamos la direccion ip de nusetro servidor.
+
+$conexion -> set_charset("utf-8");
+
+
+
+
